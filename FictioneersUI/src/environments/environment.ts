2ab812797736f46865dffac650627d5d5a466a8b
Maria <linda.en.casa@gmail.com>
2026-06-10 17:35:26 +0300

    updated environment.ts to call supabase cloud

--- FictioneersUI/src/environments/environment.ts
+++ FictioneersUI/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  /** Local Supabase (run `npx supabase start` from repo root) */
-  supabaseUrl: 'http://127.0.0.1:54321',
-  supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+  
+  supabaseUrl: 'https://hprewyefppbdwlwnpcag.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwcmV3eWVmcHBiZHdsd25wY2FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODEyNDUsImV4cCI6MjA5NjY1NzI0NX0.LBMawRT78vr4JJNAs7pakvi9PV9NUDqzAPdYcRHVp6w',
+  
 };
