5fafb27449a7899bf6580bded7397799a0757235
Maria <linda.en.casa@gmail.com>
2026-06-14 14:45:47 +0300

    neww books page implementation

--- FictioneersUI/src/app/shared/components/realm-select/realm-select.component.spec.ts
+++ FictioneersUI/src/app/shared/components/realm-select/realm-select.component.spec.ts
@@ -32,15 +32,15 @@ describe('RealmSelectComponent', () => {
     expect(fixture.nativeElement.textContent).toContain('Hard Sci-Fi');
   });
 
-  it('should emit selected slug', () => {
+  it('should emit selected realm id', () => {
     const emitted: string[] = [];
     fixture.componentInstance.valueChange.subscribe((value) => emitted.push(value));
 
     const select = fixture.nativeElement.querySelector('select') as HTMLSelectElement;
-    select.value = 'epic-fantasy';
+    select.value = '2';
     select.dispatchEvent(new Event('change'));
     fixture.detectChanges();
 
-    expect(emitted).toEqual(['epic-fantasy']);
+    expect(emitted).toEqual(['2']);
   });
 });
